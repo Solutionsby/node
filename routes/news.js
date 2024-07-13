be76d77c6f489393c67bf61587a73aedd40ba4a4
Pawel Goc <pawel.goc@solutionsby.pl>
2024-07-13 10:18:52 +0200

    News filter

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const router = express.Router();
+const News = require('../models/news')
 
 /* GET home page. */
-router.get('/',(req, res) => {
-  res.render('news', { title: 'News' });
+router.get('/',async (req, res) => {
+  try{
+    const search = req.query.search;
+    if(search){
+      const data = await News
+      .find({title:new RegExp(search, 'i')})
+      .sort({date:-1})
+      res.render('news', { title: 'News' , data, search});
+    }
+    const data = await News
+    .find()
+    .sort({date:-1})
+    res.render('news', { title: 'News' , data, search});
+  }catch (err) {
+    res.render('news', { title: 'News' });
+
+  }
 });
 
 
