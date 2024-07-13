9a67abb62758305e037c8bacb743c19abfc19e63
Pawel Goc <pawel.goc@solutionsby.pl>
2024-07-13 11:35:48 +0200

    quiz section

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
-router.get('/',(req, res) => {
-  res.render('quiz', { title: 'Quiz' });
+router.get('/', async (req, res) => {
+  let sum = 0; 
+  const show = !req.session.vote;
+  const data =await Quiz.find()
+  data.forEach((item)=>{
+    sum +=item.vote;
+  })
+
+  res.render('quiz', { title: 'Quiz', data, show, sum });
+});
+
+router.post('/', async (req, res) => {
+  try{
+    const id =req.body.quiz;
+    const data = await Quiz.findOne({_id: id})
+    data.vote = data.vote + 1;
+    data.save();
+    req.session.vote=1;
+    res.redirect('/quiz');
+  } catch(err)
+{
+  res.redirect('/quiz');
+}
+  
+  
 });
 
 
